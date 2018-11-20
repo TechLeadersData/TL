0bedb724e7286df45096d6d46ae7d0a874352826
ChegoVdrug <41072803+ChegoVdrug@users.noreply.github.com>
2018-11-20 15:38:22 +0300

    first manual commit

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -2,6 +2,7 @@ public class TestClass {
 
     public static void main(String[] args){
 
-        System.out.println("hhhhhhhhhhhh ");
+        System.out.println("yyyyyyyyyyyyyyyyy");
+        System.out.println("222222222222222");
     }
 }
