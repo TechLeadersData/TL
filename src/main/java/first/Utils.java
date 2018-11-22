3a8b0b8de24b1cee3d40d34fdd34b42dbd5b53bd
ChegoVdrug <41072803+ChegoVdrug@users.noreply.github.com>
2018-11-22 16:46:14 +0300

    new class for general tasks

---
+++ src/main/java/first/Utils.java
@@ -0,0 +1,18 @@
+package first;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Utils {
+
+    public static String getProperty(String propName) throws IOException {
+
+        Properties props = new Properties();
+        FileInputStream fis = new FileInputStream("src/main/resources/server.properties");
+        props.load(fis);
+        String prop = (props.getProperty(propName));
+        return prop;
+
+    }
+}
