927963e5e72581695661fe99cd26076f6bb6c0b0
ChegoVdrug <41072803+chegovdrug@users.noreply.github.com>
2018-11-22 22:31:21 +0300

    add BufferedInputStream close stream (only FileInputStream, is it norm?)

--- src/main/java/first/FileWorking.java
+++ src/main/java/first/FileWorking.java
@@ -1,5 +1,6 @@
 package first;
 //класс получает путь относительно коневого каталога, читает из конфига корневой каталог, возвращает запрашиваемый файл или поток
+import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 
@@ -8,11 +9,12 @@ public class FileWorking {
     public static void readFileFromServerRoot(String name) throws IOException {
 
         FileInputStream fileInputStream = new FileInputStream(Utils.getProperty("WEB_SERVER_ROOT") + "/" + name);
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
         try {
-            System.out.printf("File size: %d bytes \n", fileInputStream.available());
+            System.out.printf("File size: %d bytes \n", bufferedInputStream.available());
 
             int i = -1;
-            while ((i = fileInputStream.read()) != -1) {
+            while ((i = bufferedInputStream.read()) != -1) {
 
                 System.out.print((char) i);
                 System.out.printf(" ( код ASCII %d ) \n", i);
@@ -20,5 +22,6 @@ public class FileWorking {
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
+        fileInputStream.close();
     }
 }
